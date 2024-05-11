54198f04da815e7dc97bd66f908f61a14f96cf1a
iiebi <bruxell456@gmail.com>
2024-05-11 16:02:38 +0200

    added sql

--- app/_db/db.js
+++ app/_db/db.js
@@ -1,20 +1,21 @@
 import mysql from 'mysql2/promise';
 
-export async function query({query, values = []}){
-  // Hard-coded database connection details
-  const dbconnection = await mysql.createConnection({
-    host: '185.220.172.5',
-    database: 'ebrahim_DBlegiogloria',
-    user: 'ebrahim',
-    password: 'Rednour123'
-  });
+export default async function query({query, values = []}){
+ const dbconnection = await mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    database: process.env.MYSQL_DATABASE,
+    user:process.env.MYSQL_USER,
+    password:process.env.MYSQL_PASSWORD
+ })
 
-  try {
+ try {
     const [results] = await dbconnection.execute(query, values);
     dbconnection.end();
     return results;
-  } catch (error) {
-    console.error("Database query error:", error.message);
-    throw error; // It's usually better to throw the original error for debugging purposes
-  }
+
+ } catch (error) {
+  throw Error(error.message)
+  return { error};
+ }
+
 }
