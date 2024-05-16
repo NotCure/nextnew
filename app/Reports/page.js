4e1f310507c2757b309271a58aef8a60316ec7d5
iiebi <bruxell456@gmail.com>
2024-05-16 16:09:32 +0200

    .env test

--- app/Reports/page.js
+++ app/Reports/page.js
@@ -1,9 +1,13 @@
 "use client";
 import React, { useState } from "react";
 import Navbar from "@/app/_components/navbar/navbar";
+import dynamic from "next/dynamic";
+
 import Header from "@/app/_components/header/Header";
 import Title from "@/app/_components/title/Title";
-import ReportMap from "@/app/_components/Map/ReportMap";
+const ReportMap = dynamic(() => import("@/app/_components/Map/ReportMap"), {
+  ssr: false,
+});
 import ReportTable from "@/app/_components/Map/ReportTable";
 
 export default function Reports() {
